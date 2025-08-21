059d14d583b1146a5a9bb1b16cc19e56b7b91a78
Or Haibi <orhaibi1@gmail.com>
2025-08-21 11:51:30 +0300

    add AddChild component and service for child management

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { useAuth } from "./context/AuthContext"
-import SignupForm from "./pages/SignUpForm"
-import LoginForm from "./pages/LoginForm"
-import WhoIsPlaying from "./pages/WhoIsPlaying"
-import Home from "./pages/Home"
-
-import "./App.css"
-import ParentDashboard from "./components/ParentDashboard/ParentDashboard.jsx"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
+import SignupForm from "./pages/SignUpForm";
+import LoginForm from "./pages/LoginForm";
+import WhoIsPlaying from "./pages/WhoIsPlaying";
+import Home from "./pages/Home"; 
+import AddChild from "./pages/AddChild";  
+import "./App.css";
 
 function App() {
-  const { state, handleLogout } = useAuth()
-
-  if (screen === "parent-dashboard") {
-    return <ParentDashboard onBack={() => setScreen("home")} />
-  }
+  const { state, handleLogout } = useAuth();
+   const isAuthed = !!state.session;
 
   return (
     <BrowserRouter>
@@ -22,12 +18,14 @@ function App() {
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/who" element={<WhoIsPlaying />} />
-        <Route path="/parent" element={<ParentDashboard />} />
-        {/* <Route path="/child" element={<ChildDashboard />} /> */}
-        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+        <Route
+          path="/add"
+          element={isAuthed ? <AddChild /> : <Navigate to="/" replace />}
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
